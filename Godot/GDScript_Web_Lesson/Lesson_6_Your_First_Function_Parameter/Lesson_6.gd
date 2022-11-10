f9cd2c2e989e088f76e6d6e479b076eae9d2d9f3
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2022-11-10 18:04:26 +0800

    Update Lesson_6.gd

--- Godot/GDScript_Web_Lesson/Lesson_6_Your_First_Function_Parameter/Lesson_6.gd
+++ Godot/GDScript_Web_Lesson/Lesson_6_Your_First_Function_Parameter/Lesson_6.gd
@@ -6,3 +6,44 @@
 # move_forward(pixels) makes the object move forward over a given distance in pixels.
 # turn_right(degrees) makes the object turn on the right by a precise amount of degrees.
 # turn_left(degrees) works the same as turn_right(degrees), except the object turns to the left.
+# radians = is a unit of measurement of angles based on circle of circumference.
+#  2*pi radians = 360 degrees
+# pi radians = 180 degrees
+
+#example 1 How to Create functions with parameters
+func draw_square(length):
+    ###
+    ##
+func draw_square(side_length):
+    ###
+    ##
+
+func draw_square(size):
+    move_forward(size)
+    turn_right(90)
+    move_forward(size)
+    turn_right(90)
+    move_forward(size)
+    turn_right(90)
+    move_forward(size)
+    turn_right(90)
+
+#example 2 Function can have multiple parameters
+func name(parameters_1, parameters_2, ...):
+    ######
+
+func move_local_xy(offset_x, offset_y):
+    move_local_x(offset_x)
+    move_local_y(offset_y)
+
+#Practice 1 - Drawing Corners of Different Sizes
+func draw_corner(length):
+	move_forward(length)
+	turn_right(90)
+	move_forward(length)
+
+
+#Practice 2
+
+#Practice 3
+#Practice 4
