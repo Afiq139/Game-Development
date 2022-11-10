75c0ec30de7edb0caf555ae0d17e15c1afbf3a3f
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2022-11-10 17:05:33 +0800

    Create Lesson_6.gd

---
+++ Godot/GDScript_Web_Lesson/Lesson_6_Your_First_Function_Parameter/Lesson_6.gd
@@ -0,0 +1,8 @@
+#This is a language by game developers for game developers.
+# Can use it within Godot game engine to create games and applications. 
+#SEGA used for Godot engine to create the remake of Sonic Colors Ultimate. Engineers at Tesla use it for their cars' dashboards. 
+#https://gdquest.github.io/learn-gdscript/?ref=godot-docs#course/lesson-1-what-code-is-like/lesson.tres 
+
+# move_forward(pixels) makes the object move forward over a given distance in pixels.
+# turn_right(degrees) makes the object turn on the right by a precise amount of degrees.
+# turn_left(degrees) works the same as turn_right(degrees), except the object turns to the left.
