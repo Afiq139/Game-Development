78be662f05a6b0d6104f64da8648a930eaf607b7
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2022-11-10 16:45:10 +0800

    Update Lesson_5.gd

--- Godot/GDScript_Web_Lesson/Lesson_5_Coding_Your_First_Function/Lesson_5.gd
+++ Godot/GDScript_Web_Lesson/Lesson_5_Coding_Your_First_Function/Lesson_5.gd
@@ -6,3 +6,34 @@
 # move_forward(pixels) makes the object move forward over a given distance in pixels.
 # turn_right(degrees) makes the object turn on the right by a precise amount of degrees.
 # turn_left(degrees) works the same as turn_right(degrees), except the object turns to the left.
+
+#example-1
+func move_and_rotate():
+    move_forward(200)
+    turn_right(90)
+
+#example-2
+func draw_square():
+    move_and_rotate()
+    move_and_rotate()
+    move_and_rotate()
+    move_and_rotate()
+
+#example-3
+func draw_square(size):
+    move_forward(size)
+    turn_right(90)
+    move_forward(size)
+    turn_right(90)
+    move_forward(size)
+    turn_right(90)
+    move_forward(size)
+    turn_right(90)
+
+#example-4
+func draw_three_lines():
+    move_forward(100)
+    jump(-100,50)
+    move_forward(100)
+    jump(-100,50)
+    move_forward(100)
