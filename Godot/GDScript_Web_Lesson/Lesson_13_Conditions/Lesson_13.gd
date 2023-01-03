f620d20b50cc6a04c42c50db006c1bb1592613e1
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-03 23:57:45 +0800

    Create Lesson_13.gd

---
+++ Godot/GDScript_Web_Lesson/Lesson_13_Conditions/Lesson_13.gd
@@ -0,0 +1,70 @@
+#This is a language by game developers for game developers.
+# Can use it within Godot game engine to create games and applications. 
+#SEGA used for Godot engine to create the remake of Sonic Colors Ultimate. Engineers at Tesla use it for their cars' dashboards. 
+#https://gdquest.github.io/learn-gdscript/?ref=godot-docs#course/lesson-1-what-code-is-like/lesson.tres 
+
+# move_forward(pixels) makes the object move forward over a given distance in pixels.
+# turn_right(degrees) makes the object turn on the right by a precise amount of degrees.
+# turn_left(degrees) works the same as turn_right(degrees), except the object turns to the left.
+# radians = is a unit of measurement of angles based on circle of circumference.
+#  2*pi radians = 360 degrees
+# pi radians = 180 degrees
+
+#Example 1:
+func _process(delta):
+	rotate(4 * delta) # 4 radians per second, means how fast it is
+
+----------
+rotate(2) # Rotate 2 radians.
+move_local_x(100) # Move horizontally 100 pixels.
+move_local_y(-200) # Move vertically -200 pixels.
+
+---------
+var angular_speed = 4 # This variable is defined outside of the function
+
+func _process(delta):
+	rotate(angular_speed * delta)
+
+---------
+var angular_speed = 4 # This variable is defined outside of the function
+
+func _process(delta):
+	rotate(angular_speed * delta)
+
+func set_angular_speed(new_angular_speed):
+	angular_speed = new_angular_speed #error
+
+---------
+# variables to control movement
+var horizontal_speed = 200
+var jump_strength = 150
+var gravity = 90
+
+# variables for combat
+var health = 40
+var health_maximum = 80
+var attack_power = 10
+var defense = 4
+
+
+# Delta is the time it took Godot to complete the previous frame in seconds.
+# It's very small because frames happen many times a second.
+# It varies each frame because Godot needs to process more or less each frame.
+# delta -- time-dependent, no delta- frame-dependent
+
+#Practice 1 - Clarifying code using variables
+
+var angular_speed = 4
+
+func _process(delta):
+	rotate(angular_speed * delta)
+
+#Practice 2 - Fixing An Out Of Scope Error
+
+var angular_speed = 4
+
+func _process(delta):
+	rotate(angular_speed * delta)
+
+func set_angular_speed(new_angular_speed):
+	angular_speed = new_angular_speed
