389d2fdb51356e89c6beb6d53c5f8057fe40f389
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-02-18 05:56:12 +0800

    Create Lesson_26.gd

---
+++ Godot/GDScript_Web_Lesson/Lesson_26_Looping_over_ditionaries/Lesson_26.gd
@@ -0,0 +1,45 @@
+#This is a language by game developers for game developers.
+# Can use it within Godot game engine to create games and applications. 
+#SEGA used for Godot engine to create the remake of Sonic Colors Ultimate. Engineers at Tesla use it for their cars' dashboards. 
+#https://gdquest.github.io/learn-gdscript/?ref=godot-docs#course/lesson-1-what-code-is-like/lesson.tres 
+
+# move_forward(pixels) makes the object move forward over a given distance in pixels.
+# turn_right(degrees) makes the object turn on the right by a precise amount of degrees.
+# turn_left(degrees) works the same as turn_right(degrees), except the object turns to the left.
+# radians = is a unit of measurement of angles based on circle of circumference.
+#  2*pi radians = 360 degrees
+# pi radians = 180 degrees
+
+#Example 1:
+
+
+
+
+#Example 2:
+
+
+
+#Example 3:
+
+
+#Example 4:
+
+
+#Example 5:
+
+
+#Practice 1 - Creating An inventory Using A Dictionary
+
+var inventory = {
+    "healing heart": 3,
+    "gems": 9,
+    "sword": 1
+}
+
+
+#Practice 2 - Increasing item counts
+
+func add_item(item_name, amount):
+    inventory[item_name] += amount
+
+
