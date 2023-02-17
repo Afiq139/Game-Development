9133e673e9403b64e1a0592cfdf59dde30a4e42a
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-02-17 11:03:12 +0800

    updates

--- Godot/GDScript_Web_Lesson/Lesson_23_Appending_and_Popping_Values_from_Arrays/Lesson_23.gd
+++ Godot/GDScript_Web_Lesson/Lesson_23_Appending_and_Popping_Values_from_Arrays/Lesson_23.gd
@@ -28,9 +28,24 @@
 #Example 5:
 
 
-#Practice 1 - Converting coordinates from the grid to the screen
+#Practice 1 - Completing orders
 
- var cell_size = Vector2(80, 80)
+var waiting_orders: = []
+var completed_orders: = []
 
- func convert_to_world_coordinates(cell):
-    return cell * cell_size + cell_size/2
+#func add_order(meal_name):
+#   waiting_orders.append(meal_name)
+
+func complete_current_order():
+    var first_order = waiting_orders.pop_front()
+
+    completed_orders.append(first_order)
+
+
+#Practice 2 - Clearing up the crates
+
+var crates = ["healing heart", "shield", "gems", "sword"]
+
+func run():
+    while crates.size() > 0:
+        var crate = crates.pop_back()
